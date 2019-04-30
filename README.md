1ba1c431136e1ba4cdcaad06d247b5b25aff2a13
Carlos Baraza Haro <carlos.baraza@ieee.org>
2019-04-30 20:13:41 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Redirector
+
+Redirect traffic depending on origin. Very simple.
+
+# License
+
+MIT
---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express, { Request, Response } from 'express';
+
+const DEFAULT_URL = 'https://www.carlosbaraza.com';
+const app = express();
+
+app.get('*', (req, res) => {
+  console.log(`Redirecting "${req.hostname}" to "${DEFAULT_URL}"`);
+  res.redirect(DEFAULT_URL);
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening to http://localhost:${port}`));
